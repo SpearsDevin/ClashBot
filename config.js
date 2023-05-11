a18f95029ed11735aaa3bb2b9be97bb3e7571dfa
Devin <devinspears2004@gmail.com>
2023-05-11 11:19:14 -0500

    CWL scheduled event

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    asyncLimit: 5,
+    updateInterval: 60 * 2, // 2 Minutes
+    clans: [
+      {
+        tag: '2LLJUR0RC',
+        channelId: '931634425106686023'
+      }
+    ],
+    coc: {
+      apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhmMjJkZDJkLTgwOWUtNDE3MS1hY2JlLTI0ZDk5OGEwZDE4YyIsImlhdCI6MTY4MTgyNTYzOCwic3ViIjoiZGV2ZWxvcGVyLzM3ZDE2MTZiLTdmOGYtZWVmZi01Y2M4LTE3OTYzZjA0MDE5ZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjIwNi4yNDYuMi4xMDYiLCIyMDYuMjQ2LjUuMTgyIl0sInR5cGUiOiJjbGllbnQifV19.Uj099a8C7LJjpqoTepfxhAyOd-veHFaMUH1ssPV_BJdDEsTAxhPfIhfbTj_GMkvXfF5OY9oJVjPp2XaWz2_A-Q',
+    },
+    discord: {
+      clientId: '1102973967330983946',
+      userToken: 'MTEwMjk3Mzk2NzMzMDk4Mzk0Ng.Guo6IZ.tjBPDxX8MBKfAsdeSRB9OOVHvhSzoHi8yDdqEk'
+    },
+    mongoDB:{
+      key: 'mongodb+srv://devin:N1ght0wl@clashbot.perkjec.mongodb.net/'
+    }
+  }
+  
