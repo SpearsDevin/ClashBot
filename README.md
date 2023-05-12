93fd3be9a1f7873adc80d58165070e43f4e065da
Devin <devinspears2004@gmail.com>
2023-05-12 12:26:49 -0500

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+# ClashBot
+This discord bot uses the Clash Of Clans API to get information about a player, clan and current events. It has various commands to keep the clan up to date on whats happening.
---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    asyncLimit: 5,
+    updateInterval: 60 * 2, // 2 Minutes
+    clans: [
+      {
+        tag: '',
+        channelId: ''
+      }
+    ],
+    coc: {
+      apiKey: '',
+    },
+    discord: {
+      clientId: '',
+      userToken: ''
+    },
+    mongoDB:{
+      key: ''
+    }
+  }
+
